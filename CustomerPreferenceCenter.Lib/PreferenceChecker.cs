f1fda5285ccc61b2503f83e94eceb385feef19ce
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-10-05 22:20:38 +0100

    adding PreferenceChecker class for checking preferences

---
+++ CustomerPreferenceCenter.Lib/PreferenceChecker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace CustomerPreferenceCenter.Lib
+{
+    public class PreferenceChecker
+    {
+        public IDictionary<DateTime, ISet<Customer>> GetRecipientsForDateRange(DateTime startDate,
+            int numOfDays, IDictionary<Customer, IPreference> customerPreferences)
+        {
+            IDictionary<DateTime, ISet<Customer>> recipientsForDates = new Dictionary<DateTime, ISet<Customer>>();
+
+            DateTime endDate = startDate.AddDays(numOfDays - 1);
+
+            for(DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                IEnumerable<Customer> customers = customerPreferences
+                    .Where(customerPreference => customerPreference.Value.SendOnDate(date))
+                    .Select(customerPreference => customerPreference.Key);
+
+                recipientsForDates.Add(date, new HashSet<Customer>(customers));
+            }
+
+            return recipientsForDates;
+        }
+    }
+}
---
+++ CustomerPreferenceCenter.Tests/PreferenceCheckerTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Xunit;
+using Shouldly;
+using CustomerPreferenceCenter.Lib;
+using System.Collections.Generic;
+using System.Linq;
+namespace CustomerPreferenceCenter.Tests
+{
+    public class PreferenceCheckerTests
+    {
+        [Fact]
+        public void Preference_checker_checks_correctly()
+        {
+            IDictionary<Customer, IPreference> customerPreferences = new Dictionary<Customer, IPreference>
+            {
+                {new Customer(1, "John", "Doe"), new NeverPreference() },
+                {new Customer(2, "Jane", "Doe"), new EveryDayPreference() },
+                {new Customer(3, "Jessie", "Doe"), new DayOfMonthPreference(4) },
+                {new Customer(4, "Joe", "Doe"), new DayOfMonthPreference(10) },
+                {new Customer(5, "Julie", "Doe"), new DaysOfWeekPreference(new DayOfWeek[]{ DayOfWeek.Monday}) },
+                {new Customer(6, "Jason", "Doe"), new DaysOfWeekPreference(new DayOfWeek[]{ DayOfWeek.Monday, DayOfWeek.Thursday}) },
+            };
+
+            PreferenceChecker preferenceChecker = new PreferenceChecker();
+            DateTime startDate = new DateTime(2020, 10, 4);
+            int numOfDays = 7;
+
+            IDictionary<DateTime, ISet<Customer>> recipientsForDates =
+                preferenceChecker.GetRecipientsForDateRange(startDate, numOfDays, customerPreferences);
+
+            recipientsForDates.Count.ShouldBe(7);
+
+
+            IDictionary<DateTime, ISet<Customer>> expectedResults = new Dictionary<DateTime, ISet<Customer>>
+            {
+                {
+                    new DateTime(2020, 10, 4),
+                    new HashSet<Customer>
+                    {
+                        new Customer(2, "Jane", "Doe"),
+                        new Customer(3, "Jessie", "Doe")
+                    }
+                },
+                {
+                    new DateTime(2020, 10, 5), new HashSet<Customer>
+                    {
+                        new Customer(2, "Jane", "Doe"),
+                        new Customer(5, "Julie", "Doe"),
+                        new Customer(6, "Jason", "Doe")
+                    }
+                },
+                {
+                    new DateTime(2020, 10, 6),
+                    new HashSet<Customer>
+                    {
+                        new Customer(2, "Jane", "Doe")
+                    }
+                },
+                {
+                    new DateTime(2020, 10, 7),
+                    new HashSet<Customer>
+                    {
+                        new Customer(2, "Jane", "Doe")
+                    }
+                },
+                {
+                    new DateTime(2020, 10, 8),
+                    new HashSet<Customer>
+                    {
+                        new Customer(2, "Jane", "Doe"),
+                        new Customer(6, "Jason", "Doe")
+                    }
+                },
+                {
+                    new DateTime(2020, 10, 9),
+                    new HashSet<Customer>
+                    {
+                        new Customer(2, "Jane", "Doe")
+                    }
+                },
+                {
+                    new DateTime(2020, 10, 10),
+                    new HashSet<Customer>
+                    {
+                        new Customer(2, "Jane", "Doe"),
+                        new Customer(4, "Joe", "Doe")
+                    }
+                },
+            };
+
+            foreach(KeyValuePair<DateTime, ISet<Customer>> recipientsForDate in recipientsForDates)
+            {
+                recipientsForDate.Value.SetEquals(expectedResults[recipientsForDate.Key])
+                    .ShouldBeTrue($"Testing {recipientsForDate.Key:ddd dd/MM/yyyy}");
+            }
+
+        }
+    }
+}
