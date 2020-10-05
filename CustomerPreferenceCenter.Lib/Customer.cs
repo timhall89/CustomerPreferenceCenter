e27dd3c43f7630cbca4e25d7168d648a5c67b9ff
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-10-05 20:13:27 +0100

    Adding customer class and passing tests

---
+++ CustomerPreferenceCenter.Lib/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomerPreferenceCenter.Lib
+{
+    public class Customer
+    {
+        public Customer(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public int Id { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public override string ToString() => $"{Id} - {FirstName} {LastName}";
+    }
+}
---
+++ CustomerPreferenceCenter.Tests/CustomerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+using Shouldly;
+using CustomerPreferenceCenter.Lib;
+namespace CustomerPreferenceCenter.Tests
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void Customer_constructor_works_correctly()
+        {
+            Customer customer = new Customer(1, "John", "Doe");
+            customer.Id.ShouldBe(1);
+            customer.FirstName.ShouldBe("John");
+            customer.LastName.ShouldBe("Doe");
+        }
+
+        [Fact]
+        public void Customer_ToString_override_works()
+        {
+            Customer customer = new Customer(1, "John", "Doe");
+            customer.Id.ShouldBe(1);
+            customer.FirstName.ShouldBe("John");
+            customer.ToString().ShouldBe("1 - John Doe");
+        }
+    }
+}
