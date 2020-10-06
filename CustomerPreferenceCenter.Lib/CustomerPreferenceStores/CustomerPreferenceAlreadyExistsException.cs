58b311af834864a532c7adf212328b3c6dc7ad36
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-10-06 10:10:27 +0100

    adding CusomerPreferenceAlreadyExistsException

---
+++ CustomerPreferenceCenter.Lib/CustomerPreferenceStores/CustomerPreferenceAlreadyExistsException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CustomerPreferenceCenter.Lib.CustomerPreferenceStores
+{
+    public class CustomerPreferenceAlreadyExistsException : Exception
+    {
+        public CustomerPreferenceAlreadyExistsException(Customer customer)
+            : base($"A custoemr with id {customer.Id} already exists")
+        {
+        }
+    }
+}
--- CustomerPreferenceCenter.Lib/CustomerPreferenceStores/InMemoryCustomerPreferenceStore.cs
+++ CustomerPreferenceCenter.Lib/CustomerPreferenceStores/InMemoryCustomerPreferenceStore.cs
@@ -12,10 +12,14 @@ namespace CustomerPreferenceCenter.Lib.CustomerPreferenceStores
         public InMemoryCustomerPreferenceStore()
         {
             customerPreferences = new Dictionary<Customer, IPreference>();
+            Dictionary<int, int> i = new Dictionary<int, int>();
         }
 
         public IReadOnlyDictionary<Customer, IPreference> CustomerPreferences => (IReadOnlyDictionary<Customer, IPreference>)customerPreferences;
         public void AddCustomerPreference(Customer customer, IPreference preference)
-            => customerPreferences.Add(customer, preference);
+        {
+            if (customerPreferences.ContainsKey(customer)) throw new CustomerPreferenceAlreadyExistsException(customer);
+            customerPreferences.Add(customer, preference);
+        }
     }
 }
