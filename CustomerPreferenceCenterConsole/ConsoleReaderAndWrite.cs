04595671c8498c2486e49f067f958739eb1d1825
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-10-06 21:28:44 +0100

    adding the console application

---
+++ CustomerPreferenceCenterConsole/ConsoleReaderAndWrite.cs
@@ -0,0 +1,120 @@
+﻿using System;
+namespace CustomerPreferenceCenterConsole
+{
+    public static class ConsoleReaderAndWriter
+    {
+        private const string EXIT_COMMAND = "EXIT";
+
+        public static void _(string s) => Console.WriteLine(s);
+        public static void Warn(string s) => Console.WriteLine($"WARNING: {s}");
+
+        public static T ReadConsoleInput<T>(Func<string, T> convert)
+        {
+            while (true)
+            {
+                try
+                {
+                    return convert(ReadConsoleInput());
+                }
+                catch (FormatException ex)
+                {
+                    Warn(ex.Message);
+                }
+            }
+        }
+
+        public static T ReadConsoleInput<T>(string msg, Func<string, T> convert)
+        {
+            while (true)
+            {
+                try
+                {
+                    return convert(ReadConsoleInput(msg));
+                }
+                catch (FormatException ex)
+                {
+                    Warn(ex.Message);
+                    _("");
+                }
+            }
+        }
+
+        public static void ReadConsoleInput(Action<string> callBack)
+        {
+            while (true)
+            {
+                try
+                {
+                    callBack(ReadConsoleInput());
+                    return;
+                }
+                catch (FormatException ex)
+                {
+                    Warn(ex.Message);
+                    _("");
+                }
+            }
+        }
+
+        public static void ReadConsoleInput(string msg, Action<string> callBack)
+        {
+            while (true)
+            {
+                try
+                {
+                    callBack(ReadConsoleInput(msg));
+                    return;
+                }
+                catch (FormatException ex)
+                {
+                    Warn(ex.Message);
+                    _("");
+                }
+            }
+        }
+
+        public static string ReadConsoleInput(string msg)
+        {
+            _("");
+            if (msg != null) _(msg);
+            return ReadConsoleInput();
+        }
+
+        public static string ReadConsoleInput()
+        {
+            _("");
+            Console.Write(">> ");
+            string line = Console.ReadLine();
+            _("");
+            if (line.ToUpper().Equals(EXIT_COMMAND.ToUpper())) throw new OperationCanceledException();
+            return line.Trim();
+        }
+
+        public static void WriteTitle()
+        {
+            _("********************************************");
+            _("******** Customer Preference Center ********");
+            _("********************************************");
+            _("");
+        }
+
+        public static void ListCommands()
+        {
+            _("A - Add a new customer preference");
+            _("D - Delete a customer preference");
+            _("L - List all customer preferences");
+            _("R - Report of recipients on a date range");
+            _("H - Help, list out these apotions again");
+            _("EXIT - End the application");
+        }
+
+        public static void ListPreferenceOptions()
+        {
+            _("Enter one of the following preference options:");
+            _("E - Every day");
+            _("N - Never");
+            _("M - Specific day of the month");
+            _("W - One or more days of the week");
+        }
+    }
+}
---
+++ CustomerPreferenceCenterConsole/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using CustomerPreferenceCenter.Lib;
+using CustomerPreferenceCenter.Lib.CustomerPreferenceStores;
+using CustomerPreferenceCenter.Lib.Preferences;
+using static CustomerPreferenceCenterConsole.ConsoleReaderAndWriter;
+namespace CustomerPreferenceCenterConsole
+{
+    class Program
+    {
+        private readonly ICustomerPreferenceStore customerPreferenceStore;
+        private readonly PreferenceChecker preferenceChecker;
+
+        static void Main()
+        {
+            
+            Program program = new Program(new InMemoryCustomerPreferenceStore(), new PreferenceChecker());
+            program.Run();
+        }
+
+        public Program(ICustomerPreferenceStore customerPreferenceStore, PreferenceChecker preferenceChecker)
+        {
+            this.customerPreferenceStore = customerPreferenceStore;
+            this.preferenceChecker = preferenceChecker;
+        }
+
+        private void Run()
+        {
+            WriteTitle();
+            ListCommands();
+            try
+            {
+                while (true) ReadConsoleInput("Type a command and hit enter...", RunCommand);
+
+            }
+            catch (OperationCanceledException)
+            {
+                _("Exiting application...");
+            }
+        }
+
+        private void RunCommand(string command)
+        {
+            switch (command.ToUpper().Trim())
+            {
+                case "A":
+                    AddNewCustomerPreference();
+                    break;
+                case "D":
+                    DeleteCustomerPreference();
+                    break;
+                case "L":
+                    if (customerPreferenceStore.CustomerPreferences.Count < 1)
+                        _("-- No Preferences --");
+                    foreach (var customerPreference in customerPreferenceStore.CustomerPreferences)
+                    {
+                        _($"[{customerPreference.Key}] [{customerPreference.Value}]");
+                    }
+                    break;
+                case "R":
+                    ListRecipientsOnDates();
+                    break;
+                case "H":
+                    ListCommands();
+                    break;
+                default:
+                    throw new FormatException("Command not recognised, please try again or enter H to see a list of possible commands.");
+            }
+        }
+
+        private void DeleteCustomerPreference()
+        {
+            _("Deleteing customer preference");
+            try
+            {
+                ReadConsoleInput("Enter name of customer", name =>
+                {
+                    if(!customerPreferenceStore.CustomerPreferences.ContainsKey(name))
+                        throw new FormatException($"No preference for customer {name}, please try again");
+                    customerPreferenceStore.Remove(name);
+                });
+
+                _("Customer preference deleted");
+            }
+            catch (OperationCanceledException)
+            {
+                _("Deleting customer preference cancelled");
+            }
+        }
+
+        private void ListRecipientsOnDates()
+        {
+            _("Listing recipients on dates");
+            try
+            {
+                int numOfDays = ReadConsoleInput("Enter number of days to view", input =>
+                {
+                    if (!int.TryParse(input, out int num) || num < 1 || num > 365)
+                        throw new FormatException("Input must be a valid integer between 1 and 365, please try again");
+
+                    return num;
+                });
+
+                foreach (var x in preferenceChecker.GetRecipientsForDateRange(DateTime.Today, numOfDays, customerPreferenceStore.CustomerPreferences))
+                {
+                    _($"{x.Key:ddd dd/MM/yyyy} | {string.Join(", ", x.Value)}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                _("Listing recipients cancelled");
+            }
+        }
+
+        private void AddNewCustomerPreference()
+        {
+            _("Adding new Customer preference");
+            try
+            {
+                string customer = GetCustomer();
+                IPreference preference = GetPreference();
+                customerPreferenceStore.Add(customer, preference);
+                _($"*** Created new customer Preference : [{customer}] [{preference}]");
+            }
+            catch (OperationCanceledException)
+            {
+                _("Adding new customer preference process was exited with out being completed");
+            }
+        }
+
+        private string GetCustomer()
+            => ReadConsoleInput("Enter the customer Name",
+                name =>
+                !customerPreferenceStore.CustomerPreferences.ContainsKey(name)
+                ? name
+                : throw new FormatException($"A preference for {name} already exists, please enter a different name or exit this wizard and delete the preference for {name}"));
+
+        private IPreference GetPreference()
+        {
+            ListPreferenceOptions();
+            return ReadConsoleInput(GetPreference);
+        }
+
+        private IPreference GetPreference(string option)
+        {
+            IPreference preference;
+            switch (option.ToUpper())
+            {
+                case "E":
+                    preference = new EveryDayPreference();
+                    break;
+                case "N":
+                    preference = new NeverPreference();
+                    break;
+                case "M":
+                    string dayOfMonthMessage = "Enter the day of month (1 - 28), Any number less than 1 will be taken as 1 and any number greater than 28 will be taken as 28";
+                    preference = ReadConsoleInput(dayOfMonthMessage, DayOfMonthPreference.Parse);
+                    break;
+                case "W":
+                    _(@"Enter a comma separated list of one or more weekday numbers, from 1 = Sunday to 7 = Saturday, example ""1,4,5""");
+                    _("if any values are not a number between 1 and 7 the whole input will fail");
+                    preference = ReadConsoleInput(DaysOfWeekPreference.Parse);
+                    break;
+                default:
+                    throw new FormatException($"Option {option} is not valid, please try again");
+
+            }
+
+            return preference;
+        }
+    }
+}
