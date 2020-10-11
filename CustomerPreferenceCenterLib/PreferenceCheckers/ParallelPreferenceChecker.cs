2084f2f4ab6c57145bc3fd4bbc34ff1a023b0a81
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-10-11 19:40:51 +0100

    using concurrent dictionary

--- CustomerPreferenceCenterLib/PreferenceCheckers/ParallelPreferenceChecker.cs
+++ CustomerPreferenceCenterLib/PreferenceCheckers/ParallelPreferenceChecker.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using CustomerPreferenceCenterLib.Preferences;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace CustomerPreferenceCenterLib.PreferenceCheckers
 {
@@ -11,13 +12,14 @@ namespace CustomerPreferenceCenterLib.PreferenceCheckers
         public IReadOnlyDictionary<DateTime, ISet<string>> GetRecipientsForDateRange(DateTime startDate,
             int numOfDays, IReadOnlyDictionary<string, IPreference> customerPreferences)
         {
-            Dictionary<DateTime, ISet<string>> recipientsForDates = new Dictionary<DateTime, ISet<string>>(numOfDays);
+            ConcurrentDictionary<DateTime, ISet<string>> recipientsForDates = new ConcurrentDictionary<DateTime, ISet<string>>(10, numOfDays);
             List<Task> tasks = new List<Task>(numOfDays);
             DateTime endDate = startDate.AddDays(numOfDays - 1);
-            for(DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
             {
                 DateTime _date = date;
-                if(recipientsForDates.TryAdd(_date, null))
+                if (recipientsForDates.TryAdd(_date, null))
                 {
                     Task t = Task.Run(() =>
                     {
@@ -25,15 +27,15 @@ namespace CustomerPreferenceCenterLib.PreferenceCheckers
                         .Where(customerPreference => customerPreference.Value.SendOnDate(_date))
                         .Select(customerPreference => customerPreference.Key);
 
-                        recipientsForDates[_date] = new HashSet<string>(customers);
+                        recipientsForDates.TryUpdate(_date, new HashSet<string>(customers), null);
                     });
 
                     tasks.Add(t);
-                }
+                };
+               
             }
-
             Task.WaitAll(tasks.ToArray());
-            return recipientsForDates;
+            return (IReadOnlyDictionary < DateTime, ISet<string> > )recipientsForDates;
         }
     }
 }
